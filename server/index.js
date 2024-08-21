3502df79ae3600716d09357acdf316e4e8b5c60f
addymwenda12 <muneneaddy@gmail.com>
2024-08-21 18:37:40 +0300

    chore: Add CORS middleware to enable cross-origin resource sharing

--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import chatRoutes from './routes/chat.js';
 
 dotenv.config();
+
+app.use(cors());
 
 const app = express();
 const port = process.env.PORT || 3000;
